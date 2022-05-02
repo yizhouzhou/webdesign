5818e1995723e23eb10404d065fcb57be805170e
yizhouzhou <zhouy@huntington.edu>
2022-05-01 23:57:19 -0500

    all changed project! add some new function!

---
+++ src/main/java/models/ApartmentModels.java
@@ -0,0 +1,42 @@
+package models;
+
+public class ApartmentModels {
+
+    String apartmentinfo ;
+    String aparttotalbedroom ;
+    String apartprice ;
+
+    public ApartmentModels(String apartmentinfo, String aparttotalbedroom, String apartprice) {
+        this.apartmentinfo = apartmentinfo;
+        this.aparttotalbedroom = aparttotalbedroom;
+        this.apartprice = apartprice;
+    }
+
+
+    public String getApartmentinfo() {
+        return apartmentinfo;
+    }
+
+    public void setApartmentinfo(String apartmentinfo) {
+        this.apartmentinfo = apartmentinfo;
+    }
+
+    public String getAparttotalbedroom() {
+        return aparttotalbedroom;
+    }
+
+    public void setAparttotalbedroom(String aparttotalbedroom) {
+        this.aparttotalbedroom = aparttotalbedroom;
+    }
+
+    public String getApartprice() {
+        return apartprice;
+    }
+
+    public void setApartprice(String apartprice) {
+        this.apartprice = apartprice;
+    }
+
+
+
+}
